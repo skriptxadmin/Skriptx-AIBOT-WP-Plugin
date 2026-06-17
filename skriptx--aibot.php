fae4c24de05b4af76b6218c3140646c680259ed4
Alaksandar Jesus <alaksandarjesus@yahoo.co.in>
2026-06-17 09:13:07 +0530

    Update licensing information

--- skriptx--aibot.php
+++ skriptx--aibot.php
@@ -8,7 +8,9 @@
  * Author URI: https://skriptx.com
  * Support URI: https://support.skriptx.com
  * Text Domain: skriptx-aibot
- * License: Properaitory
+ * Stable tag: 1.0.0
+ * License: GPLv2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  */
 
 if (! defined('ABSPATH')) {
